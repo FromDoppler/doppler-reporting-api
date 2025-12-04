69d868fb96435118e47b066bf424125fa3af4f06
emoralesms <emorales@makingsense.com>
2025-12-04 17:50:39 -0300

    chore: test 4

--- Doppler.ReportingApi/Infrastructure/CampaignRepository.cs
+++ Doppler.ReportingApi/Infrastructure/CampaignRepository.cs
@@ -348,7 +348,7 @@ namespace Doppler.ReportingApi.Infrastructure
                         ,C.[IdCampaign]
                         ,C.[UTCScheduleDate]
                         ,ISNULL(C.[AmountSubscribersToSend],0) [Subscribers]
-                        ,ISNULL(C.[AmountSentSubscribers],0) [Sent]
+                        ,(ISNULL(C.[DistinctOpenedMailCount],0) + ISNULL(C.[UnopenedMailCount],0)) AS [Sent]
                         ,ISNULL(C.[DistinctOpenedMailCount],0) [Opens]
                         ,ISNULL(C.[DistinctClickCount],0) [Clicks]
                         ,ISNULL(C.[HardBouncedMailCount],0) [Hard]
