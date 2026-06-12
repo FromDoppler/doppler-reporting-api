e2b3dc7b3a1e6ab18491d10354e4068391cd0764
emoralesms <emorales@makingsense.com>
2026-06-12 12:38:49 -0300

    feat: get super user token inside repository

--- Doppler.ReportingApi/Services/PushContact/PushContactService.cs
+++ Doppler.ReportingApi/Services/PushContact/PushContactService.cs
@@ -1,19 +1,16 @@
 using System;
 using System.Threading.Tasks;
 using Doppler.ReportingApi.Models;
-using Doppler.ReportingApi.Services.SuperUserToken;
 
 namespace Doppler.ReportingApi.Services.PushContact
 {
     public class PushContactService : IPushContactService
     {
         private readonly IPushContactSdk _pushContactSdk;
-        private readonly ISuperUserTokenService _superUserTokenService;
 
-        public PushContactService(IPushContactSdk pushContactSdk, ISuperUserTokenService superUserTokenService)
+        public PushContactService(IPushContactSdk pushContactSdk)
         {
             _pushContactSdk = pushContactSdk;
-            _superUserTokenService = superUserTokenService;
         }
 
         public Task<DomainStatsPerDayModel> GetDomainStatsPerDayAsync(
@@ -21,13 +18,10 @@ namespace Doppler.ReportingApi.Services.PushContact
             DateTime startDate,
             DateTime endDate)
         {
-            var token = _superUserTokenService.GenerateToken();
-
             return _pushContactSdk.GetDomainStatsPerDayAsync(
                 name,
                 startDate,
-                endDate,
-                token);
+                endDate);
         }
     }
 }
